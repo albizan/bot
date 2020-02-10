653b79545afcd7aa8d2c641c22b82f62ccaaecd8
unknown <albizan@hotmail.it>
2020-02-10 23:23:25 +0100

    Cleaned Code

---
+++ wizards/sell/index.js
@@ -0,0 +1,44 @@
+const WizardScene = require('telegraf/scenes/wizard');
+
+const {
+  getSellItemWizardPrompt,
+  getPaymentMethodsPrompt,
+  generateCaption,
+  generatePaymentsInlineKeyboard,
+} = require('../../helper');
+
+const {
+  askForTitle,
+  validateTitle,
+  confirmTitleAndAskForDescription,
+  validateDescription,
+  confirmDescriptionAndAskForImages,
+  validateImagesAndAskForPrice,
+  priceValidation,
+  priceConfirmationAndShowPaymentsKeyboard,
+  updatePaymentMethods,
+} = require('./steps');
+
+const { SELL_ITEM_WIZARD } = require('../../types/scenes.types');
+
+// A wizard is a special type of scene
+const sellItemWizard = new WizardScene(
+  // Wizard's name
+  SELL_ITEM_WIZARD,
+  askForTitle,
+  validateTitle,
+  confirmTitleAndAskForDescription,
+  validateDescription,
+  confirmDescriptionAndAskForImages,
+  validateImagesAndAskForPrice,
+  priceValidation,
+  priceConfirmationAndShowPaymentsKeyboard,
+  updatePaymentMethods
+);
+
+sellItemWizard.leave(ctx =>
+  ctx.reply(
+    'Alla prossima, ricorda di scrivere /start se vuoi iniziare da capo la procedura'
+  )
+);
+module.exports = sellItemWizard;
