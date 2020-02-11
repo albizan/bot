7d1645aba023a351f92088d1bb1048b370ed9ee1
unknown <albizan@hotmail.it>
2020-02-11 21:59:15 +0100

    Add SOS

--- emoji.js
+++ emoji.js
@@ -1,10 +1,11 @@
 module.exports = {
-  package: "\u{1F4E6}",
-  memo: "\u{1F4DD}",
-  moneyBag: "\u{1F4B0}",
-  moneyFly: "\u{1F4B8}",
-  silhouette: "\u{1F464}",
-  checkMark: "\u{2705}",
-  back: "\u{25C0}",
-  forward: "\u{25B6}"
+  package: '\u{1F4E6}',
+  memo: '\u{1F4DD}',
+  moneyBag: '\u{1F4B0}',
+  moneyFly: '\u{1F4B8}',
+  silhouette: '\u{1F464}',
+  checkMark: '\u{2705}',
+  back: '\u{25C0}',
+  forward: '\u{25B6}',
+  sos: '\u{1F198}',
 };
