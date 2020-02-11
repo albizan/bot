ad55ac3fbce7a58a7f225cffec63d778261e792c
unknown <albizan@hotmail.it>
2020-02-11 23:30:09 +0100

    forward messages

--- scenes/chat.scene.js
+++ scenes/chat.scene.js
@@ -1,13 +1,18 @@
 const Scene = require('telegraf/scenes/base');
-const Markup = require('telegraf/markup');
-
-// Greeter scene
+const { sos } = require('../emoji');
+// Chat scene
 const chat = new Scene('chat');
-chat.enter(ctx =>
+chat.enter(async ctx => {
+  await ctx.reply(`${sos}<b>MODALITA CHAT ATTIVATA</b>${sos}`, {
+    parse_mode: 'HTML',
+  });
   ctx.reply(
-    'Modalità chat avviata\nDa adesso in avanti tutti i tuoi messaggi verranno inoltrati agli admin. Eventuali risposte e comunicazioni dagli amministratori ti verranno mostrate qui nel bot\nPer uscire dalla modalità chat digitare il comando /quit'
-  )
-);
+    `Da adesso in avanti tutti i tuoi messaggi verranno inoltrati agli admin. Eventuali risposte e comunicazioni dagli amministratori ti verranno mostrate qui nel bot\nPer uscire dalla modalità chat digitare il comando <i>/quit</i>`,
+    {
+      parse_mode: 'HTML',
+    }
+  );
+});
 chat.leave(ctx =>
   ctx.reply(
     'Sei uscito dalla modalità admin\nScrivi /start per iniziare la procedura'
@@ -15,12 +20,11 @@ chat.leave(ctx =>
 );
 chat.hears(/quit/gi, ctx => ctx.scene.leave());
 chat.on('message', async ctx => {
-  if (ctx.message && ctx.message.text) {
-    await ctx.telegram.sendMessage(
-      process.env.SECRET_CHAT_ID,
-      `${ctx.from.id}\n@${ctx.from.username} ha scritto:\n${ctx.message.text}\n`
-    );
-  }
+  ctx.forwardMessage(
+    process.env.SECRET_CHAT_ID,
+    ctx.from.id,
+    ctx.message.message_id
+  );
 });
 
 module.exports = chat;
