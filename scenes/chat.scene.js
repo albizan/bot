c2c41a9634095f1d6128303f0b639c9acd74e9f1
Alberto <albizan@hotmail.it>
2020-02-13 18:56:11 +0100

    Check if user is muted

--- scenes/chat.scene.js
+++ scenes/chat.scene.js
@@ -1,30 +1,57 @@
 const Scene = require('telegraf/scenes/base');
+const knex = require('../db');
+const { SUPPORT_CHAT_SCENE } = require('../types/scenes.types');
+const { startMenuMarkup } = require('../helper');
+const logger = require('../logger');
 const { sos } = require('../emoji');
+
 // Chat scene
-const chat = new Scene('chat');
-chat.enter(async ctx => {
-  await ctx.reply(`${sos}<b>MODALITA CHAT ATTIVATA</b>${sos}`, {
-    parse_mode: 'HTML',
-  });
+const supportChat = new Scene(SUPPORT_CHAT_SCENE);
+supportChat.enter(async ctx => {
+  const { id } = ctx.from;
+  knex('users')
+    .where({ id })
+    .then(rows => {
+      if (rows[0].muted) {
+        ctx.reply(`${sos}<b>CHAT DISATTIVATA DAGLI ADMIN</b>${sos}`, {
+          parse_mode: 'HTML',
+        });
+        ctx.scene.leave();
+      } else {
+        ctx.reply(`${sos} <b>CHAT DI SUPPORTO</b> ${sos}`, {
+          parse_mode: 'HTML',
+        });
+        ctx.reply(
+          `Da adesso in avanti tutti i tuoi messaggi verranno inoltrati agli admin. Eventuali risposte e comunicazioni dagli amministratori ti verranno mostrate qui nel bot\nPer uscire dalla modalità chat di supporto digitare il comando <i>/quit</i>`,
+          {
+            parse_mode: 'HTML',
+          }
+        );
+      }
+    });
+});
+supportChat.leave(async ctx => {
+  await ctx.reply('Sei uscito dalla chat di supporto');
   ctx.reply(
-    `Da adesso in avanti tutti i tuoi messaggi verranno inoltrati agli admin. Eventuali risposte e comunicazioni dagli amministratori ti verranno mostrate qui nel bot\nPer uscire dalla modalità chat digitare il comando <i>/quit</i>`,
+    `Ciao ${ctx.from.first_name}\nBenvenuto nel mercatino del gruppo "PC Building Italia"`,
     {
-      parse_mode: 'HTML',
+      reply_markup: startMenuMarkup,
     }
   );
 });
-chat.leave(ctx =>
-  ctx.reply(
-    'Sei uscito dalla modalità admin\nScrivi /start per iniziare la procedura'
-  )
-);
-chat.hears(/quit/gi, ctx => ctx.scene.leave());
-chat.on('message', async ctx => {
-  ctx.forwardMessage(
-    process.env.SECRET_CHAT_ID,
-    ctx.from.id,
-    ctx.message.message_id
-  );
+supportChat.hears(/quit/gi, ctx => ctx.scene.leave());
+supportChat.on('message', async ctx => {
+  knex('users')
+    .where({ id: ctx.from.id })
+    .then(rows => {
+      if (!rows[0].muted) {
+        ctx.forwardMessage(
+          process.env.SECRET_CHAT_ID,
+          ctx.from.id,
+          ctx.message.message_id
+        );
+      }
+    });
 });
 
-module.exports = chat;
+module.exports = supportChat;
