c2005f605b1c3f6ff84420cf2caffaa630059d67
Alberto <albizan@hotmail.it>
2020-02-13 18:55:35 +0100

    Save users in db on start

--- index.js
+++ index.js
@@ -7,34 +7,35 @@ if (process.env.NODE_ENV === 'development') {
 const Telegraf = require('telegraf');
 const knex = require('./db');
 // Import types
-const { SELL_ITEM, SEEK_ITEM } = require('./types/callbacks.types');
-const { SELL_ITEM_WIZARD, SEEK_ITEM_WIZARD } = require('./types/scenes.types');
+const {
+  SELL_ITEM,
+  SEEK_ITEM,
+  SUPPORT_CHAT,
+} = require('./types/callbacks.types');
+const {
+  SELL_ITEM_WIZARD,
+  SEEK_ITEM_WIZARD,
+  SUPPORT_CHAT_SCENE,
+} = require('./types/scenes.types');
 
 // Import Wizards
 const sellItemWizard = require('./wizards/sell');
 
-const chat = require('./scenes/chat.scene');
+// Imports Scenes
+const supportChat = require('./scenes/chat.scene');
 
 // Import logger
 const logger = require('./logger');
 
-const { Markup, Stage, session } = Telegraf;
+const { Stage, session } = Telegraf;
+
+// import markups
+const { startMenuMarkup } = require('./helper');
 
 // Compose stage with given scenes
-const stage = new Stage([chat, sellItemWizard]);
+const stage = new Stage([supportChat, sellItemWizard]);
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
-const startMessage = Markup.inlineKeyboard([
-  [
-    Markup.callbackButton('Ricerca', SEEK_ITEM),
-    Markup.callbackButton('Vendita', SELL_ITEM),
-  ],
-  [Markup.callbackButton('Chatta con gli admin', 'admin')],
-])
-  .oneTime()
-  .resize()
-  .extra();
 
 bot.use(session());
 bot.use(stage.middleware());
@@ -43,11 +44,19 @@ bot.start(ctx => {
   const { first_name, id, username } = ctx.from;
   logger.info(`${username} started Nas BOT`);
 
+  // Save new user to the database
+  knex('users')
+    .insert({ id, muted: false })
+    .then(`${id} added to the database`)
+    .catch(err => logger.error(err.detail));
+
   // When user starts bot, show welcome message in private chat and asks for type of action
   ctx.telegram.sendMessage(
     id,
-    `Ciao ${first_name}\nBenvenuto nel mercatino del gruppo "PC Building Italia"`,
-    startMessage
+    `Ciao ${first_name}\n\nBenvenuto nel mercatino del gruppo "PC Building Italia"`,
+    {
+      reply_markup: startMenuMarkup,
+    }
   );
 });
 
@@ -56,10 +65,12 @@ bot.action(SELL_ITEM, Stage.enter(SELL_ITEM_WIZARD));
 bot.action(SEEK_ITEM, ctx => {
   // Delete previous inline message to avoid cluttering the chat
   ctx.deleteMessage(ctx.callbackQuery.message.message_id);
-  ctx.reply('Questa funzionalità non è ancora disponibile', startMessage);
+  ctx.reply('Questa funzionalità non è ancora disponibile', {
+    reply_markup: startMenuMarkup,
+  });
 });
 
-bot.action('admin', Stage.enter('chat'));
+bot.action(SUPPORT_CHAT, Stage.enter(SUPPORT_CHAT_SCENE));
 
 bot.help(ctx => {
   ctx.reply('Premi /start per iniziare la procedura');
@@ -71,6 +82,40 @@ bot.on('message', ctx => {
     ctx.message.reply_to_message.forward_from
   ) {
     const { id } = ctx.message.reply_to_message.forward_from;
+    if (ctx.message.text === '/mute') {
+      knex('users')
+        .where({ id })
+        .update({ muted: 'true' })
+        .then(rows => {
+          logger.info(`${id} was mutated`);
+          ctx.reply(`${id} è stato mutato`);
+        })
+        .catch(err => logger.error(err));
+      try {
+        ctx.telegram.sendMessage(
+          id,
+          '<b>Da questo momento non potrai piu inviare messaggi agli admin</b>',
+          {
+            parse_mode: 'HTML',
+          }
+        );
+      } catch (error) {
+        ctx.reply('Impossibile inviare messaggio');
+      } finally {
+        return;
+      }
+    }
+    if (ctx.message.text === '/unmute') {
+      knex('users')
+        .where({ id })
+        .update({ muted: 'false' })
+        .then(rows => {
+          logger.info(`${id} is now unmuted`);
+          ctx.reply(`${id} è stato smutato`);
+        })
+        .catch(err => console.error(err));
+      return;
+    }
     try {
       ctx.telegram.sendMessage(id, ctx.message.text);
     } catch (error) {
