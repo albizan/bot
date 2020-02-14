9f8937efa7962f2da5c2716f468f5ec0b168ed3b
unknown <albizan@hotmail.it>
2020-02-14 22:41:01 +0100

    load env from .env in development

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,9 @@
+const dotenv = require('dotenv');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config();
+}
+
 module.exports = {
   client: 'pg',
   connection: process.env.DB_CONNECTION_STRING,
