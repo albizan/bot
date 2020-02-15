053408d20d0d6579fb604c348180214b4fbcb42c
unknown <albizan@hotmail.it>
2020-02-15 12:02:07 +0100

    Use winston logger

--- logger.js
+++ logger.js
@@ -1,6 +1,26 @@
-const pino = require("pino");
-const logger = pino({
-  name: "nasbot",
-  prettyPrint: true
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [],
 });
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+if (process.env.NODE_ENV === 'production') {
+  logger.add(
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  );
+}
 module.exports = logger;
