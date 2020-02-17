32353aa32b67a701f167d44822d3af8c946b333a
Alberto <albizan@hotmail.it>
2020-02-17 22:22:34 +0100

    create columns title and user_id

--- db/migrations/20200214134718_sale_announcement.js
+++ db/migrations/20200214134718_sale_announcement.js
@@ -1,16 +1,17 @@
 exports.up = function(knex) {
   return knex.schema.createTable('sale_announcements', function(table) {
     table
-      .string('id')
+      .increments('id')
       .notNullable()
       .primary();
-    table.string('user_id').notNullable();
+    table.string('title').notNullable();
     table.string('category').notNullable();
     table.string('url').nullable();
     table
       .boolean('isRemoved')
       .defaultTo(false)
       .notNullable();
+    table.string('user_id').notNullable();
 
     table
       .foreign('user_id')
