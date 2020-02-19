fc6eba5854d53e27a93cbe7188c3f4bfee8a3596
Alberto <albizan@hotmail.it>
2020-02-19 19:16:48 +0100

    Delete inline menu to clean chat

--- bot.js
+++ bot.js
@@ -100,6 +100,7 @@ bot.start(async ctx => {
 // Handle middlewares for callback_data
 bot.action(SELL_ITEM, ctx => {
   ctx.answerCbQuery();
+  ctx.deleteMessage(ctx.callbackQuery.message.message_id);
   ctx.scene.enter(SELL_ITEM_WIZARD);
 });
 
