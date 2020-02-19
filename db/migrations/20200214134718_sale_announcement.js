97b4bd9052de80ea53b0732d22bb0c8b1603b789
Alberto <albizan@hotmail.it>
2020-02-20 00:03:43 +0100

    Add text column

--- db/migrations/20200214134718_sale_announcement.js
+++ db/migrations/20200214134718_sale_announcement.js
@@ -7,6 +7,7 @@ exports.up = function(knex) {
     table.string('product').notNullable();
     table.string('category').notNullable();
     table.string('url').nullable();
+    table.text('images').notNullable();
     table
       .boolean('isRemoved')
       .defaultTo(false)
