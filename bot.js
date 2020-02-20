7d956fca68b34212637715ae66a5b8b2a3d2c23e
Alberto <albizan@hotmail.it>
2020-02-20 22:02:49 +0100

    Import and use setupCommands function

--- bot.js
+++ bot.js
@@ -2,6 +2,9 @@ const Telegraf = require('telegraf');
 const { Stage, session } = Telegraf;
 const Markup = require('telegraf/markup');
 const knex = require('./db');
+
+// Import command setups
+const setupCommands = require('./commands');
 // Import types
 const {
   SELL_ITEM,
@@ -39,7 +42,9 @@ const logger = require('./logger');
 const { startMenuMarkup } = require('./helper');
 
 // Define administrators, id must be numbers and not strings
-const admins = process.env.ADMINS.split(',').map(admin => parseInt(admin));
+process.env.ADMINS = process.env.ADMINS.split(',').map(admin =>
+  parseInt(admin)
+);
 
 // Compose stage with given scenes
 const stage = new Stage([supportChat, sellItemWizard, seekItemWizard]);
@@ -48,54 +53,6 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.use(session());
 bot.use(stage.middleware());
-
-bot.start(async ctx => {
-  const { id, username, first_name } = ctx.from;
-  try {
-    // When user starts bot, show welcome message in private chat and show menu
-    ctx.telegram.sendMessage(
-      id,
-      `Ciao <b>${first_name}</b>\n\nBenvenuto nel mercatino del gruppo <i>"PC Building Italia"</i>`,
-      {
-        reply_markup: startMenuMarkup,
-        parse_mode: 'HTML',
-      }
-    );
-  } catch (err) {
-    ctx.reply(
-      'Sono un BOT, non posso contattarti in privato se prima non vai su @nas_bot_test e clicchi su avvia'
-    );
-    console.log(err);
-  }
-  logger.info(`${username} started the Bot`);
-
-  // Retrieve user id from id
-  let users = [];
-  try {
-    users = await knex('users').where({ id });
-  } catch (err) {
-    logger.error('Cannot retrieve users from database');
-    console.log(err);
-  }
-  // If user is not in the database, save it
-  if (users.length === 0) {
-    try {
-      const insertedUserId = await knex('users')
-        .returning('id')
-        .insert({
-          id,
-          username,
-          first_name,
-        });
-      logger.info(`${insertedUserId} saved to the database`);
-    } catch (err) {
-      logger.error('Cannot insert in db');
-      console.log(err);
-    }
-  } else {
-    logger.info(`${users[0].id} is already present in the database`);
-  }
-});
 
 // Handle middlewares for callback_data
 bot.action(SELL_ITEM, ctx => {
@@ -174,199 +131,11 @@ bot.action(SUPPORT_CHAT, ctx => {
 });
 
 // Administration Commands
-bot.command('mute', ctx => {
-  const { id } = ctx.from;
-  if (!admins.includes(id)) {
-    return;
-  }
-  // If admin specifies the id with command /mute 123456789
-  if (!ctx.message.reply_to_message) {
-    const idToMute = ctx.message.text.split(' ')[1];
-    if (!idToMute) {
-      return;
-    }
-    knex('users')
-      .where({ id: idToMute })
-      .then(rows => {
-        if (rows.length === 0) {
-          ctx.reply('Non è stato trovato nessun utente');
-        } else {
-          knex('users')
-            .where({ id: idToMute })
-            .update({ muted: true })
-            .then(() => {
-              logger.info(`${id} is now muted`);
-              try {
-                ctx.reply(`L'utente con id ${idToMute} è stato mutato`);
-                ctx.telegram.sendMessage(
-                  idToMute,
-                  '<b>Da questo momento non potrai piu inviare messaggi agli admin</b>',
-                  {
-                    parse_mode: 'HTML',
-                    reply_markup: startMenuMarkup,
-                  }
-                );
-              } catch (error) {
-                ctx.reply('Impossibile inviare messaggio');
-              }
-            });
-        }
-      });
-    return;
-  }
-  const { forward_from } = ctx.message.reply_to_message;
-  if (!forward_from) {
-    ctx.reply(
-      "Impossibile ottenere l'id dell'utente, mutare manualmente l'utente con il comando /mute id_utente"
-    );
-  }
-  if (forward_from) {
-    const { id } = forward_from;
-    knex('users')
-      .where({ id })
-      .update({ muted: 'true' })
-      .then(() => {
-        logger.info(`${id} is now muted`);
-        try {
-          ctx.reply(`${id} è stato mutato`);
-          ctx.telegram.sendMessage(
-            id,
-            '<b>Da questo momento non potrai piu inviare messaggi agli admin</b>',
-            {
-              parse_mode: 'HTML',
-              reply_markup: startMenuMarkup,
-            }
-          );
-        } catch (error) {
-          ctx.reply('Impossibile inviare messaggio');
-        }
-      })
-      .catch(err => logger.error(err));
-  }
-});
-
-bot.command('unmute', ctx => {
-  const { id } = ctx.from;
-  if (!admins.includes(id)) {
-    return;
-  }
-
-  // If admin specifies the id with command /unmute 123456789
-  if (!ctx.message.reply_to_message) {
-    const idToUnmute = ctx.message.text.split(' ')[1];
-    if (!idToUnmute) {
-      return;
-    }
-    knex('users')
-      .where({ id: idToUnmute })
-      .then(rows => {
-        if (rows.length === 0) {
-          ctx.reply('Non è stato trovato nessun utente');
-        } else {
-          knex('users')
-            .where({ id: idToUnmute })
-            .update({ muted: false })
-            .then(() => {
-              try {
-                ctx.reply(`L'utente con id ${idToUnmute} è stato smutato`);
-              } catch (error) {
-                ctx.reply('Impossibile inviare messaggio');
-              }
-            });
-        }
-      });
-    return;
-  }
-  const { forward_from } = ctx.message.reply_to_message;
-  if (!forward_from) {
-    ctx.reply(
-      "Impossibile ottenere l'id dell'utente, smutare manualmente l'utente con il comando /unmute id_utente"
-    );
-  }
-  if (forward_from) {
-    const { id } = forward_from;
-    knex('users')
-      .where({ id })
-      .update({ muted: 'false' })
-      .then(() => {
-        logger.info(`${id} è stato smutato`);
-        ctx.reply(`${id} è stato smutato`);
-      })
-      .catch(err => logger.error(err));
-  }
-});
-
-bot.command('approve', async ctx => {
-  const { id } = ctx.from;
-  if (!admins.includes(id)) {
-    return;
-  }
-
-  const { reply_to_message } = ctx.message;
-  if (!reply_to_message) {
-    ctx.reply('Questo comando deve essere usato come risposta ad un annuncio');
-    return;
-  }
-
-  if (!reply_to_message.caption) {
-    return;
-  }
-  // Get announce id from caption's entities
-  const entity = reply_to_message.caption_entities[2];
-  const announceId = reply_to_message.caption.substring(
-    // offset + 2 in order to remove '#av'
-    entity.offset + 3,
-    entity.offset + entity.length
-  );
-
-  // Retieve images of the sale announce from the database
-  let image_ids, saleAnnounce;
-  try {
-    const result = await knex('sale_announcements')
-      .select('images')
-      .first()
-      .where({ id: announceId });
-    image_ids = result.images.split(',');
-  } catch (error) {
-    console.log('Cannot retreive images for current announce');
-  }
-  // generate again array of inputMediaPhoto to be sent with sendMediaGroup to channel
-  const media = image_ids.map(file_id => {
-    return {
-      type: 'photo',
-      media: file_id,
-    };
-  });
-  media[0].caption = reply_to_message.caption;
-  try {
-    saleAnnounce = await ctx.telegram.sendMediaGroup(
-      process.env.CHANNEL_USERNAME,
-      media
-    );
-    // generate url
-    const url = `https://t.me/${process.env.CHANNEL_USERNAME.slice(1)}/${
-      saleAnnounce[0].message_id
-    }`;
-
-    await knex('sale_announcements')
-      .where({ id: announceId })
-      .update({ url });
-
-    // Update dabase with newly created url
-    ctx.reply(url);
-  } catch (error) {
-    console.log(error);
-    ctx.reply(
-      'Errore, impossibile inviare il tuo messaggio. Riprova piu tardi'
-    );
-    ctx.reply(error.message);
-    return ctx.scene.leave();
-  }
-});
+setupCommands(bot);
 
 bot.on('message', async ctx => {
   const { id } = ctx.from;
-  if (!admins.includes(id)) {
+  if (!process.env.ADMINS.includes(id)) {
     return;
   }
   const { text, reply_to_message } = ctx.message;
