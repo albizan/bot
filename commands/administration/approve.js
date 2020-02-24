54b8de1af89c2ba28b7c85752bf23b2591d50b85
Alberto <albizan@hotmail.it>
2020-02-24 17:22:10 +0100

    Retrieve images for current insertion

--- commands/administration/approve.js
+++ commands/administration/approve.js
@@ -1,3 +1,5 @@
+const knex = require('../../db');
+
 function setupApproveCommand(bot) {
   bot.command('approve', async ctx => {
     const { id } = ctx.from;
@@ -18,25 +20,24 @@ function setupApproveCommand(bot) {
     }
     // Get announce id from caption's entities
     const entity = reply_to_message.caption_entities[2];
-    const announceId = reply_to_message.caption.substring(
-      // offset + 2 in order to remove '#av'
+    const insertionId = reply_to_message.caption.substring(
+      // offset + 3 in order to remove '#av'
       entity.offset + 3,
       entity.offset + entity.length
     );
 
-    // Retieve images of the sale announce from the database
-    let image_ids, saleAnnounce;
+    // Retieve insertion's images from db
+    let image_ids, insertion;
     try {
-      const result = await knex('insertions')
-        .select('images')
-        .first()
-        .where({ id: announceId });
-      image_ids = result.images.split(',');
+      image_ids = await knex('images')
+        .select('file_id')
+        .where({ insertion_id: insertionId });
     } catch (error) {
-      console.log('Cannot retreive images for current announce');
+      console.log(error);
+      console.log('Cannot retreive images for current insertion');
     }
     // generate again array of inputMediaPhoto to be sent with sendMediaGroup to channel
-    const media = image_ids.map(file_id => {
+    const media = image_ids.map(({ file_id }) => {
       return {
         type: 'photo',
         media: file_id,
@@ -44,27 +45,26 @@ function setupApproveCommand(bot) {
     });
     media[0].caption = reply_to_message.caption;
     try {
-      saleAnnounce = await ctx.telegram.sendMediaGroup(
+      insertion = await ctx.telegram.sendMediaGroup(
         process.env.CHANNEL_USERNAME,
         media
       );
       // generate url
       const url = `https://t.me/${process.env.CHANNEL_USERNAME.slice(1)}/${
-        saleAnnounce[0].message_id
+        insertion[0].message_id
       }`;
 
+      // Update dabase with newly created url
       await knex('insertions')
-        .where({ id: announceId })
+        .where({ id: insertionId })
         .update({ url });
 
-      // Update dabase with newly created url
       ctx.reply(url);
     } catch (error) {
       console.log(error);
       ctx.reply(
         'Errore, impossibile inviare il tuo messaggio. Riprova piu tardi'
       );
-      ctx.reply(error.message);
       return ctx.scene.leave();
     }
   });
