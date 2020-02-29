9a1bc97aac35686e6c9198faf3c7a14aa94114f0
Alberto <albizan@hotmail.it>
2020-02-29 17:50:39 +0100

    Change message on leaving

--- wizards/sellProduct/index.js
+++ wizards/sellProduct/index.js
@@ -33,7 +33,19 @@ const sellProductWizard = new WizardScene(
   updatePaymentMethods
 );
 
-sellProductWizard.leave(ctx =>
-  ctx.reply('Benvenuto nel mercatino', { reply_markup: startMenuMarkup })
-);
+sellProductWizard.leave(ctx => {
+  try {
+    ctx.telegram.sendMessage(
+      ctx.from.id,
+      `Ciao <b>${ctx.from.first_name}</b>\n\nBenvenuto nel BOT ufficiale del gruppo <a href="https://t.me/joinchat/BUc_2U-1GRQClo4MllBuFA">MIT - Mercatino Informatica e Tecnologia</a>\n\nTi ricordo che in qualunque momento puoi inviare il comando /home per aprire il <i>Menu Principale</i>\nQuesto bot ti permette di creare annunci di vendita per le tue componenti informatiche e non solo. Tutti gli annunci, prima di essere pubblicati sul canale ufficiale @mitvendita, verranno valutati ed eventualmente approvati dallo <b>STAFF</b>`,
+      {
+        reply_markup: startMenuMarkup,
+        parse_mode: 'HTML',
+      }
+    );
+  } catch (error) {
+    ctx.reply(error.message);
+    return;
+  }
+});
 module.exports = sellProductWizard;
