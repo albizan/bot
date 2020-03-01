3bf80f527d65659be5678cc7053a4352c1829307
Alberto <albizan@hotmail.it>
2020-03-01 02:14:16 +0100

    Add quit command to leave wizard

--- wizards/newInsertion/index.js
+++ wizards/newInsertion/index.js
@@ -1,5 +1,5 @@
 const WizardScene = require('telegraf/scenes/wizard');
-const { startMenuMarkup } = require('../../helper');
+const { startMenuMarkup, getWelcomeMessage } = require('../../helper');
 
 const {
   askForCategory,
@@ -33,18 +33,19 @@ const sellProductWizard = new WizardScene(
   updatePaymentMethods
 );
 
+sellProductWizard.command(['home', 'quit'], ctx => {
+  return ctx.scene.leave();
+});
+
 sellProductWizard.leave(ctx => {
+  const { id, first_name } = ctx.from;
   try {
-    ctx.telegram.sendMessage(
-      ctx.from.id,
-      `Ciao <b>${ctx.from.first_name}</b>\n\nBenvenuto nel BOT ufficiale del gruppo <a href="https://t.me/joinchat/BUc_2U-1GRQClo4MllBuFA">MIT - Mercatino Informatica e Tecnologia</a>\n\nTi ricordo che in qualunque momento puoi inviare il comando /home per aprire il <i>Menu Principale</i>\nQuesto bot ti permette di creare annunci di vendita per le tue componenti informatiche e non solo. Tutti gli annunci, prima di essere pubblicati sul canale ufficiale @mitvendita, verranno valutati ed eventualmente approvati dallo <b>STAFF</b>`,
-      {
-        reply_markup: startMenuMarkup,
-        parse_mode: 'HTML',
-      }
-    );
+    ctx.telegram.sendMessage(id, getWelcomeMessage(first_name), {
+      reply_markup: startMenuMarkup,
+      parse_mode: 'HTML',
+    });
   } catch (error) {
-    ctx.reply(error.message);
+    logger.error(error);
     return;
   }
 });
