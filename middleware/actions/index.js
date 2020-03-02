c5df977bb38e23cb629987f99e2fc8bfd86abdb4
Alberto <albizan@hotmail.it>
2020-03-02 23:23:00 +0100

    Deleted import

--- middleware/actions/index.js
+++ middleware/actions/index.js
@@ -1,14 +1,12 @@
 const setupHome = require('./home');
 const setupNewInsertion = require('./sellProduct.js');
 const setupSearchInsertionByCategory = require('./searchInsertionByCategory');
-const setupFindProductsByCategory = require('./findProductByCategory');
 const setupBotInfo = require('./botInfo');
 
 const setupActions = bot => {
   setupHome(bot);
   setupNewInsertion(bot);
   setupSearchInsertionByCategory(bot);
-  setupFindProductsByCategory(bot);
   setupBotInfo(bot);
 };
 
