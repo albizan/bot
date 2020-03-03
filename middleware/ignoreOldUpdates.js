f80cad40e4212bdee7d704b8152fa171474b4b9e
Alberto <albizan@hotmail.it>
2020-03-03 12:05:05 +0100

    ignore old callback queries

--- middleware/ignoreOldUpdates.js
+++ middleware/ignoreOldUpdates.js
@@ -1,12 +1,25 @@
 const ignoreOldUpdates = async (ctx, next) => {
-  if (ctx.updateType === 'message') {
-    if (new Date().getTime() / 1000 - ctx.message.date < 60) {
+  const { updateType } = ctx;
+  let updateDate;
+  switch (updateType) {
+    case 'message':
+      updateDate = ctx.message.date;
+      if (new Date().getTime() / 1000 - updateDate < 60) {
+        next();
+      } else {
+        console.log(`Ignoring message/callback_query from ${ctx.from.id} - message is too old`);
+      }
+      break;
+    case 'callback_query':
+      updateDate = ctx.update.callback_query.message.date;
+      if (new Date().getTime() / 1000 - updateDate < 60) {
+        next();
+      } else {
+        console.log(`Ignoring message/callback_query from ${ctx.from.id} - message is too old`);
+      }
+      break;
+    default:
       next();
-    } else {
-      console.log(`Ignoring message from ${ctx.from.id} - message is too old`);
-    }
-  } else {
-    next();
   }
 };
 
