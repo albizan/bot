97237fcac470d4b75a3b3d28c1e462da3352c5c3
Alberto <albizan@hotmail.it>
2020-03-03 12:11:34 +0100

    add waiting threeshold

--- middleware/ignoreOldUpdates.js
+++ middleware/ignoreOldUpdates.js
@@ -1,21 +1,22 @@
 const ignoreOldUpdates = async (ctx, next) => {
   const { updateType } = ctx;
-  let updateDate;
+  let updateDate,
+    waitingThreshold = 60;
   switch (updateType) {
     case 'message':
       updateDate = ctx.message.date;
-      if (new Date().getTime() / 1000 - updateDate < 60) {
+      if (new Date().getTime() / 1000 - updateDate < waitingThreshold) {
         next();
       } else {
-        console.log(`Ignoring message/callback_query from ${ctx.from.id} - message is too old`);
+        console.log(`Ignoring message from ${ctx.from.id} - message is too old`);
       }
       break;
     case 'callback_query':
       updateDate = ctx.update.callback_query.message.date;
-      if (new Date().getTime() / 1000 - updateDate < 60) {
+      if (new Date().getTime() / 1000 - updateDate < waitingThreshold) {
         next();
       } else {
-        console.log(`Ignoring message/callback_query from ${ctx.from.id} - message is too old`);
+        console.log(`Ignoring callback_query from ${ctx.from.id} - message is too old`);
       }
       break;
     default:
