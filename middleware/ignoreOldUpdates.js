79d9d89377186cc553b4be2e98227fcdca2c0aed
Alberto <albizan@hotmail.it>
2020-03-17 17:40:32 +0100

    code cleanup

--- middleware/ignoreOldUpdates.js
+++ middleware/ignoreOldUpdates.js
@@ -1,7 +1,7 @@
 const ignoreOldUpdates = async (ctx, next) => {
   const { updateType } = ctx;
-  let updateDate,
-    waitingThreshold = 60;
+  let updateDate;
+  const waitingThreshold = 60;
   switch (updateType) {
     case 'message':
       updateDate = ctx.message.date;
@@ -11,16 +11,6 @@ const ignoreOldUpdates = async (ctx, next) => {
         console.log(`Ignoring message from ${ctx.from.id} - message is too old`);
       }
       break;
-    /*
-    case 'callback_query':
-      updateDate = ctx.update.callback_query.message.date;
-      if (new Date().getTime() / 1000 - updateDate < waitingThreshold) {
-        next();
-      } else {
-        console.log(`Ignoring callback_query from ${ctx.from.id} - message is too old`);
-      }
-      break;
-    */
     default:
       next();
   }
