53e6f5908ea23ee8555ff624554ce62b3bd2dae9
Alberto <albizan@hotmail.it>
2020-03-18 13:10:27 +0100

    Users can give only a rating without text

--- commands/administration/feedback.js
+++ commands/administration/feedback.js
@@ -6,27 +6,29 @@ function setupFeedbackCommand(bot) {
     // Get user
     const issuers = await getUserById(ctx.from.id);
     if (issuers.length === 0) {
-      ctx.reply(`@${ctx.from.username} non trovato nel database`);
+      ctx.reply(`Per rilasciare un feedback, avvia prima il bot`);
       return;
     }
     const { args } = ctx.state.command;
-    let [username, rate, ...text] = args;
-    if (!username) {
-      console.log('Username not found');
+    if (args.length === 0) {
       ctx.reply(
-        'Per lasciare un feedback:\n<code>/feedback @nomeutente rating testo_del_feedback</code>\n\nPer vedere i feedback:\n<code>/feedback @nomeutente</code>',
+        'Per lasciare un feedback:\n<em>/feedback @nomeutente rating testo_del_feedback</em>\n\nPer vedere i feedback:\n<em>/feedback @nomeutente</em>',
         {
           parse_mode: 'HTML',
         }
       );
       return;
     }
+
+    let [username, rate, ...text] = args;
     username = username.startsWith('@') ? username.slice(1) : username;
     const user = await getUserFromUsername(username);
     if (!user) {
-      ctx.reply('Utente non trovato');
+      ctx.reply(`Errore: @${username} non è presente nel database`);
       return;
     }
+
+    // Just one arg
     if (args.length === 1) {
       const res = await getValidatedFeedbacksByUser(user.id);
       const avg = parseFloat(res[0].avg).toFixed(1);
@@ -36,19 +38,60 @@ function setupFeedbackCommand(bot) {
         return;
       }
       ctx.reply(`Utente: @${username}\nNumero di feedback: ${count}\nRating:${avg}/5`);
-    } else if (args.length === 2) {
+      return;
+    }
+
+    // More than one
+    if (!isRateValid(rate, ctx)) {
+      return;
+    }
+    text = text === undefined ? '' : text.join(' ');
+
+    const issuer_id = ctx.from.id;
+    let receiver_id;
+    try {
+      const user = await getUserFromUsername(username);
+      if (!user) {
+        ctx.reply(`@${username} non trovato nel database`);
+        return;
+      }
+      receiver_id = user.id;
+    } catch (error) {
+      console.log(error);
+      ctx.reply('Errore db');
+    }
+
+    try {
+      const { id, feedback_issuer, feedback_receiver, feedback_rate, feedback_text } = await upsert({
+        table: 'feedbacks',
+        object: { feedback_receiver: receiver_id, feedback_issuer: issuer_id, feedback_rate: rate, feedback_text: text },
+        constraint: '(id)',
+      });
+      const message = `#FB${id}\nDa: @${ctx.from.username} - ${feedback_issuer}\nA: @${username} - ${feedback_receiver}\nRating: ${feedback_rate}/5${
+        feedback_text === '' ? '' : `\nFeedback: ${feedback_text}`
+      }`;
+      try {
+        ctx.telegram.sendMessage(process.env.SECRET_CHAT_ID, message, {
+          reply_markup: Markup.inlineKeyboard([[Markup.callbackButton('Approva Feedback', 'approve_feedback')]]),
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    /*
+else if (args.length === 2) {
+      // Solo un rating senza testo
+      if (!isRateValid(rate, ctx)) {
+        return;
+      }
     } else {
-      rate = parseInt(rate);
-      if (isNaN(rate)) {
-        console.log('Rate is not a number');
-        ctx.reply("Inserisci un voto da 1 a 5 dopo l'username");
+      if (!isRateValid(rate, ctx)) {
         return;
       }
-      if (rate > 5 || rate < 0) {
-        ctx.reply('Il voto deve essere compreso tra 0 e 5');
-        return;
-      }
-      text = text.join(' ');
+      text = text === undefined ? '' : text.join(' ');
       if (!text) {
         ctx.reply('Inserisci un testo');
         return;
@@ -85,7 +128,22 @@ function setupFeedbackCommand(bot) {
         console.log(error);
       }
     }
+    */
   });
+}
+
+function isRateValid(rate, ctx) {
+  rate = parseInt(rate);
+  if (isNaN(rate)) {
+    ctx.reply("Inserisci un voto da 1 a 5 dopo l'username");
+    return false;
+  }
+  if (rate > 5 || rate < 0) {
+    ctx.reply('Il voto deve essere compreso tra 0 e 5');
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = setupFeedbackCommand;
